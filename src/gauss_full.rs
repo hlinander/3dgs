9e7cf44e531983f656cefb4e09783dedebb74984
Hampus Linander <hampus.linander@gmail.com>
2024-06-14 20:57:26 -0700

    3d splats might work

--- src/gauss_full.rs
+++ src/gauss_full.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "metal")]
 extern crate accelerate_src;
 
-use candle_core::{DType, Device};
+use candle_core::{DType, Device, IndexOp};
 use candle_nn::{Optimizer, VarBuilder, VarMap};
 #[cfg(feature = "duckdb")]
 use duckdb::{Connection, Result};
@@ -125,9 +125,12 @@ impl FastSplats {
             n_splats,
             pos: vb
                 .get_with_hints(
-                    &[n_splats, 2],
+                    &[n_splats, 3],
                     "pos",
-                    candle_nn::init::DEFAULT_KAIMING_NORMAL,
+                    candle_nn::init::Init::Randn {
+                        mean: 0.0,
+                        stdev: 1.0,
+                    },
                 )
                 .unwrap(),
             color: vb
@@ -139,32 +142,37 @@ impl FastSplats {
                 )
                 .unwrap(),
             scale: vb
-                .get_with_hints(&[n_splats, 2], "scale", candle_nn::init::Init::Const(scale))
+                .get_with_hints(&[n_splats, 3], "scale", candle_nn::init::Init::Const(scale))
                 .unwrap(),
 
             quat: vb
                 .get_with_hints(
                     &[n_splats, 4],
                     "quat",
-                    candle_nn::init::DEFAULT_KAIMING_NORMAL,
+                    // candle_nn::init::DEFAULT_KAIMING_NORMAL,
+                    candle_nn::init::Init::Randn {
+                        mean: 0.0,
+                        stdev: 1.0,
+                    },
                 )
                 .unwrap(),
         }
     }
 
     fn rot(&self, device: &Device) -> candle_core::Result<candle_core::Tensor> {
-        let M2 = candle_core::Tensor::new(
-            &[
-                // r,   i,   j,   k
-                [1.0f32, 0.0, 0.0, 0.0], // r
-                [0.0, 0.0, 0.0, 0.0],    // i
-                [0.0, 0.0, 0.0, 0.0],    // j
-                [0.0, 0.0, 0.0, 1.0],    // k
-            ],
-            device,
-        )?;
-        let quat = M2.broadcast_matmul(&self.quat.unsqueeze(2)?)?;
-        let two_s = (2.0 / (&quat * &quat)?.sum(1)?)?;
+        // let M2 = candle_core::Tensor::new(
+        //     &[
+        //         // r,   i,   j,   k
+        //         [1.0f32, 0.0, 0.0, 0.0], // r
+        //         [0.0, 0.0, 0.0, 0.0],    // i
+        //         [0.0, 0.0, 0.0, 0.0],    // j
+        //         [0.0, 0.0, 0.0, 1.0],    // k
+        //     ],
+        //     device,
+        // )?;
+        // let quat = M2.broadcast_matmul(&self.quat.unsqueeze(2)?)?;
+        let quat = &self.quat.unsqueeze(2)?;
+        let two_s = (2.0 / (quat * quat)?.sum(1)?)?;
         let two_s = two_s.expand(&[self.n_splats, 9])?;
         let bquat = quat
             .unsqueeze(1)?
@@ -192,21 +200,21 @@ impl FastSplats {
             candle_core::Tensor::new(&[1.0f32, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0], device);
         let x = (lhs?.unsqueeze(0)?.expand(&[self.n_splats, 9]) - ((-1.0) * two_s_M)?)?;
         let x = x.reshape(&[self.n_splats, 3, 3])?;
-        let proj = candle_core::Tensor::new(&[[1.0f32, 0.0, 0.0], [0.0, 1.0, 0.0]], device)?;
-        let proj = proj
-            .unsqueeze(0)?
-            .expand(&[self.n_splats, 2, 3])?
-            .contiguous()?;
-        let x = proj.matmul(&x.matmul(&proj.t()?)?)?;
+        // let proj = candle_core::Tensor::new(&[[1.0f32, 0.0, 0.0], [0.0, 1.0, 0.0]], device)?;
+        // let proj = proj
+        //     .unsqueeze(0)?
+        //     .expand(&[self.n_splats, 2, 3])?
+        //     .contiguous()?;
+        // let x = proj.matmul(&x.matmul(&proj.t()?)?)?;
         Ok(x)
     }
 
     fn cov(&self, device: &Device) -> candle_core::Result<candle_core::Tensor> {
         let R = self.rot(device)?;
-        let m = candle_core::Tensor::eye(2, DType::F32, device)?;
+        let m = candle_core::Tensor::eye(3, DType::F32, device)?;
         let scale_shift = candle_core::Tensor::exp(&self.scale)?;
-        let scale = (m.unsqueeze(0)?.expand(&[self.n_splats, 2, 2])?
-            * scale_shift.unsqueeze(1)?.expand(&[self.n_splats, 2, 2]))?;
+        let scale = (m.unsqueeze(0)?.expand(&[self.n_splats, 3, 3])?
+            * scale_shift.unsqueeze(1)?.expand(&[self.n_splats, 3, 3]))?;
         let scale = scale.contiguous()?;
         R.matmul(&(&scale * &scale)?.matmul(&R.t()?)?)
     }
@@ -217,10 +225,69 @@ impl FastSplats {
         device: &Device,
     ) -> candle_core::Result<candle_core::Tensor> {
         let resolution = self.resolution;
-        let pos = self.pos.unsqueeze(1)?.unsqueeze(1)?;
+
+        // 3D splats
+        let pos = self.pos.unsqueeze(2)?;
+        let cov3d = self.cov(device)?;
+
+        let camera_pos = candle_core::Tensor::new(&[0.0f32, 0.0, 5.0], device)?;
+
+        let W = candle_core::Tensor::new(
+            &[[1.0f32, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
+            device,
+        )?;
+
+        // [N, 3, 3]
+        let camera_pos_expanded = camera_pos
+            .unsqueeze(0)?
+            .expand(&[self.n_splats, 3])?
+            .contiguous()?;
+
+        // [N, 3, 3]
+        let W_expanded = W
+            .unsqueeze(0)?
+            .expand(&[self.n_splats, 3, 3])?
+            .contiguous()?;
+
+        // [N, 3, 1]
+        let u = (W_expanded.matmul(&pos)? + camera_pos_expanded.unsqueeze(2)?)?;
+        let u2 = u
+            .i((0..self.n_splats, 2..3, 0..1))?
+            .expand((self.n_splats, 2, 1))?
+            .contiguous()?;
+        let pos = (u.i((0..self.n_splats, 0..2, 0..1))? / u2)?.squeeze(2)?;
+        /*
+        J = | 1/u2 0    -u0/u2^2 |
+            | 0    1/u2 -u1/u2^2 |
+            | u0/u u1/u u2/u     |
+         */
+        // println!("{}", u.i((0, 0..3, 0))?);
+        // let J = self.create_J(&u, device)?;
+        let J = self.create_Jinv(&u, device)?;
+        // println!("{:?}", test.shape());
+        // println!("J: {}", J.i((0, 0..3, 0..3))?);
+        // println!("Jinv: {}", J_inv.i((0, 0..3, 0..3))?);
+        // let test = J.matmul(&J_inv)?;
+        // println!("{:?}", test.shape());
+        // println!("{}", test.i((10, 0..3, 0..3))?);
+        // panic!();
+
+        let cov_transformed = J
+            .t()?
+            .matmul(&W_expanded.matmul(&cov3d.matmul(&W_expanded.t()?.matmul(&J)?)?)?)?;
+
+        let C = cov_transformed
+            .i((0..self.n_splats, 0..2, 0..2))?
+            .contiguous()?;
+        // println!("{:?}", u2.shape());
+        // panic!();
+        // let C = candle_core::Tensor::zeros((1,), DType::F32, device)?;
+
+        // 2D splats
+        let pos = pos.unsqueeze(1)?.unsqueeze(1)?;
+        // let pos = pos.unsqueeze(1)
         let dist = (&self.screen - pos.broadcast_as(&[self.n_splats, resolution, resolution, 2]))?
             .contiguous()?;
-        let C = self.cov(device)?;
         let d2 = dist.reshape((self.n_splats, (), 2, 1))?;
         let c2 = C
             .unsqueeze(1)?
@@ -239,26 +306,106 @@ impl FastSplats {
         let c6 = c6.sum(0)?;
         Ok(c6)
     }
+
+    fn create_J(
+        &self,
+        u: &candle_core::Tensor,
+        device: &Device,
+    ) -> Result<candle_core::Tensor, candle_core::Error> {
+        let one_over_u2 = (1.0 / u.i((0..self.n_splats, 2..3, 0..1))?.contiguous()?)?;
+        let one_over_u2_squared = (&one_over_u2 * &one_over_u2)?;
+        let u0 = u.i((0..self.n_splats, 0..1, 0..1))?.contiguous()?;
+        let u1 = u.i((0..self.n_splats, 1..2, 0..1))?.contiguous()?;
+        let u2 = u.i((0..self.n_splats, 2..3, 0..1))?.contiguous()?;
+        let one_over_u_norm =
+            (1.0 / ((u * u)?.sum_keepdim(1)?.contiguous()?).sqrt()?)?.contiguous()?;
+        let J = candle_core::Tensor::zeros((self.n_splats, 3, 3), DType::F32, device)?;
+        let J = J.slice_assign(&[0..self.n_splats, 0..1, 0..1], &one_over_u2)?;
+        let J = J.slice_assign(&[0..self.n_splats, 1..2, 1..2], &one_over_u2)?;
+        let J = J.slice_assign(
+            &[0..self.n_splats, 0..1, 2..3],
+            &(-1.0 * (&u0 * &one_over_u2_squared)?)?,
+        )?;
+        let J = J.slice_assign(
+            &[0..self.n_splats, 1..2, 2..3],
+            &(-1.0 * (&u1 * &one_over_u2_squared)?)?,
+        )?;
+        let J = J.slice_assign(&[0..self.n_splats, 2..3, 0..1], &(&u0 * &one_over_u_norm)?)?;
+        let J = J.slice_assign(&[0..self.n_splats, 2..3, 1..2], &(&u1 * &one_over_u_norm)?)?;
+        let J = J.slice_assign(&[0..self.n_splats, 2..3, 2..3], &(&u2 * &one_over_u_norm)?)?;
+        let J = J.contiguous()?;
+        Ok(J)
+    }
+    fn create_Jinv(
+        &self,
+        u: &candle_core::Tensor,
+        device: &Device,
+    ) -> Result<candle_core::Tensor, candle_core::Error> {
+        // let one_over_u2 = (1.0 / u.i((0..self.n_splats, 2..3, 0..1))?.contiguous()?)?;
+        // let one_over_u2_squared = (&one_over_u2 * &one_over_u2)?;
+        let u0 = u.i((0..self.n_splats, 0..1, 0..1))?.contiguous()?;
+        let u1 = u.i((0..self.n_splats, 1..2, 0..1))?.contiguous()?;
+        let u2 = u.i((0..self.n_splats, 2..3, 0..1))?.contiguous()?;
+        let one_over_u_norm_squared = (1.0 / (u * u)?.sum_keepdim(1)?)?.contiguous()?;
+        // let one_over_u_norm = one_over_u_norm_squared.sqrt()?;
+        let one_over_u_norm =
+            (1.0 / ((u * u)?.sum_keepdim(1)?.contiguous()?).sqrt()?)?.contiguous()?;
+        let J = candle_core::Tensor::zeros((self.n_splats, 3, 3), DType::F32, device)?;
+        let J = J.slice_assign(
+            &[0..self.n_splats, 0..1, 0..1],
+            &((&u2 * ((&u1 * &u1)? + (&u2 * &u2)?)?)? / &one_over_u_norm_squared)?,
+        )?;
+        let J = J.slice_assign(
+            &[0..self.n_splats, 1..2, 1..2],
+            &((&u2 * ((&u0 * &u0)? + (&u2 * &u2)?)?)? / &one_over_u_norm_squared)?,
+        )?;
+        let J = J.slice_assign(
+            &[0..self.n_splats, 0..1, 1..2],
+            &(-1.0 * ((&u0 * (&u1 * &u2)?)? / &one_over_u_norm_squared)?)?,
+        )?;
+        let J = J.slice_assign(
+            &[0..self.n_splats, 1..2, 0..1],
+            &(-1.0 * ((&u0 * (&u1 * &u2)?)? / &one_over_u_norm_squared)?)?,
+        )?;
+        let J = J.slice_assign(
+            &[0..self.n_splats, 2..3, 0..1],
+            &(-1.0 * ((&u0 * (&u2 * &u2)?)? / &one_over_u_norm_squared)?)?,
+        )?;
+        let J = J.slice_assign(
+            &[0..self.n_splats, 2..3, 1..2],
+            &(-1.0 * ((&u1 * (&u2 * &u2)?)? / &one_over_u_norm_squared)?)?,
+        )?;
+        // Last column
+        let J = J.slice_assign(&[0..self.n_splats, 0..1, 2..3], &(u0 / &one_over_u_norm)?)?;
+        let J = J.slice_assign(&[0..self.n_splats, 1..2, 2..3], &(u1 / &one_over_u_norm)?)?;
+        let J = J.slice_assign(&[0..self.n_splats, 2..3, 2..3], &(u2 / &one_over_u_norm)?)?;
+        let J = J.contiguous()?;
+        Ok(J)
+    }
 }
 
-fn train_splats(
-    target: candle_core::Tensor,
+struct TrainSplatsConfig {
     n_splats_per_batch: usize,
     n_batches: usize,
     initial_scale: f64,
     epochs: usize,
+}
+
+fn train_splats(
+    target: candle_core::Tensor,
+    config: TrainSplatsConfig,
     device: &Device,
 ) -> candle_core::Result<Vec<FastSplats>> {
     let res = target.shape().dims()[0];
     let varmap: VarMap = VarMap::new();
     let vs = VarBuilder::from_varmap(&varmap, DType::F32, &device);
-    let splats = (0..n_batches)
+    let splats = (0..config.n_batches)
         .map(|idx| {
             FastSplats::new(
                 &vs.pp(format!("batch_{}", idx)),
-                n_splats_per_batch,
+                config.n_splats_per_batch,
                 res,
-                initial_scale,
+                config.initial_scale,
                 device,
             )
         })
@@ -274,9 +421,11 @@ fn train_splats(
     )?;
     #[cfg(feature = "duckdb")]
     let db_connection = db::init_db();
+    // panic!();
     let mut next_log = Instant::now();
-    for epoch in 0..epochs {
-        for active_idx in 0..n_batches {
+    let mut first_screen = None;
+    for epoch in 0..config.epochs {
+        for active_idx in 0..config.n_batches {
             let mut screen = candle_core::Tensor::zeros((res, res), DType::F32, device)?;
             for (batch_idx, batch) in splats.iter().enumerate() {
                 let batch_screen = batch.render(device)?;
@@ -287,6 +436,10 @@ fn train_splats(
                 }
             }
             let screen = candle_nn::ops::sigmoid(&screen)?;
+            if first_screen.is_none() {
+                first_screen = Some(true);
+                save_image(&screen, "fist.png")?;
+            }
             let loss = candle_nn::loss::mse(&screen, &target)?;
             let grads = loss.backward()?;
             optimizer.step(&grads)?;
@@ -379,11 +532,21 @@ fn main() -> candle_core::Result<()> {
             Err(_) => candle_core::Device::Cpu,
         },
     };
+    let device = candle_core::Device::Cpu;
     println!("{:?}", device);
 
     // let target = generate_target_from_splats(&device)?;
-    let target = load_image("deepfield.png", 64, &device)?;
+    let target = load_image("simple.png", 64, &device)?;
     save_image(&target, "target.png")?;
-    let trained_splats = train_splats(target, 100, 1, 3.0, 100000, &device)?;
+    let trained_splats = train_splats(
+        target,
+        TrainSplatsConfig {
+            n_splats_per_batch: 100,
+            n_batches: 1,
+            initial_scale: -3.0,
+            epochs: 100000,
+        },
+        &device,
+    )?;
     Ok(())
 }
